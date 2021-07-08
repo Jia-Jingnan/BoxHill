ea702dd60adec0ca47ac86db977ddcebaefa6d33
Jia-Jingnan <875480307@qq.com>
2021-07-08 02:16:01 -0800

    0708-在run模块中调用HttpRequest类，调用service函数，发送get和post请求

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
 # Author:   jingnan
 # Date:    2021/7/6
 # Desc: 执行入口
+from utils.http_util import HttpRequest
+
+
+def run():
+    httpRequest = HttpRequest()
+
+    login_url = 'http://localhost:8089/user/login.do'
+    login_data = {'username': 'admin', 'password': 'admin'}
+    get_category_url = 'http://localhost:8089/manage/category/get_category.do?categoryId=100001'
+
+    login_res = httpRequest.service(login_url, 'post', login_data)
+    print(login_res.status_code)
+    print(login_res.json())
+
+    get_category_res = httpRequest.service(get_category_url, 'get', cookies=login_res.cookies)
+    print(get_category_res.json())
+
+
+run()
+
--- utils/http_util.py
+++ utils/http_util.py
@@ -23,29 +23,6 @@ class HttpRequest:
         return response
 
 
-if __name__ == '__main__':
-
-    httpRequest = HttpRequest()
-
-    login_url = 'http://localhost:8089/user/login.do'
-    login_data = {'username': 'admin', 'password': 'admin'}
-    get_category_url = 'http://localhost:8089/manage/category/get_category.do?categoryId=100001'
-
-    login_res = httpRequest.service(login_url, 'post', login_data)
-    print(login_res.status_code)
-    print(login_res.json())
-
-    get_category_res = httpRequest.service(get_category_url, 'get', cookies=login_res.cookies)
-    print(get_category_res.json())
-
-    # # 创建一个会话
-    # session = requests.session()
-    # # 在会话中进行登陆
-    # login_res = session.post(login_url, params=login_data)
-    # # 在会话中执行其他需要登陆的接口，可以不用在传递cookie，因为在这个会话中已经执行过登陆接口
-    # get_category_res_2 = session.get(get_category_url)
-    # print(get_category_res_2.json())
-
 
 
 
