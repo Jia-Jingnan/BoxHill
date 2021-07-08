24652584767248a0c9e006652ce495084d44f879
Jia-Jingnan <875480307@qq.com>
2021-07-08 02:03:06 -0800

    0708-抽象为一个类，通过service方法发送get和post请求，并在main()中进行测试

--- utils/http_util.py
+++ utils/http_util.py
@@ -4,28 +4,47 @@
 import requests
 
 
-login_url = 'http://localhost:8089/user/login.do'
-login_data = {'username': 'admin', 'password': 'admin'}
-get_category_url = 'http://localhost:8089/manage/category/get_category.do?categoryId=100001'
+class HttpRequest:
 
-login_res = requests.post(login_url, login_data)
-print(login_res.status_code)
-print(login_res.text)
-print(login_res.json())
-print(login_res.cookies)
-# header必须是字典格式
-print(login_res.request.headers)
+    def service(self, url, method, data=None, cookies=None):
+        try:
+            if method.upper() == 'GET':
+                response = requests.get(url, data, cookies=cookies)
 
-get_category_res = requests.get(get_category_url, cookies=login_res.cookies)
-print(get_category_res.json())
+            elif method.upper() == 'POST':
+                response = requests.post(url, data, cookies=cookies)
 
-# 创建一个会话
-session = requests.session()
-# 在会话中进行登陆
-login_res = session.post(login_url, params=login_data)
-# 在会话中执行其他需要登陆的接口，可以不用在传递cookie，因为在这个会话中已经执行过登陆接口
-get_category_res_2 = session.get(get_category_url)
-print(get_category_res_2.json())
+            else:
+                print('Bad Request！')
+        except Exception as e:
+            print('Bad Request:{}'.format(e))
+            raise e
+
+        return response
+
+
+if __name__ == '__main__':
+
+    httpRequest = HttpRequest()
+
+    login_url = 'http://localhost:8089/user/login.do'
+    login_data = {'username': 'admin', 'password': 'admin'}
+    get_category_url = 'http://localhost:8089/manage/category/get_category.do?categoryId=100001'
+
+    login_res = httpRequest.service(login_url, 'post', login_data)
+    print(login_res.status_code)
+    print(login_res.json())
+
+    get_category_res = httpRequest.service(get_category_url, 'get', cookies=login_res.cookies)
+    print(get_category_res.json())
+
+    # # 创建一个会话
+    # session = requests.session()
+    # # 在会话中进行登陆
+    # login_res = session.post(login_url, params=login_data)
+    # # 在会话中执行其他需要登陆的接口，可以不用在传递cookie，因为在这个会话中已经执行过登陆接口
+    # get_category_res_2 = session.get(get_category_url)
+    # print(get_category_res_2.json())
 
 
 
