f2253989236efedfacd2a0168ecc244d34e59aa4
Jia-Jingnan <875480307@qq.com>
2021-07-09 02:36:26 -0800

    0709-修改run函数，参数化执行登陆接口

--- run.py
+++ run.py
@@ -3,21 +3,27 @@
 # Desc: 执行入口
 from utils.http_util import HttpRequest
 
-
-def run():
-    httpRequest = HttpRequest()
-
-    login_url = 'http://localhost:8089/user/login.do'
-    login_data = {'username': 'admin', 'password': 'admin'}
-    get_category_url = 'http://localhost:8089/manage/category/get_category.do?categoryId=100001'
-
-    login_res = httpRequest.service(login_url, 'post', login_data)
-    print(login_res.status_code)
-    print(login_res.json())
-
-    get_category_res = httpRequest.service(get_category_url, 'get', cookies=login_res.cookies)
-    print(get_category_res.json())
+# 登陆接口测试用例
+# cases = [
+#     {'http://localhost:8089/user/login.do', 'post', {'usename': 'admin', 'password': 'admin'}, '0'},
+#     {'http://localhost:8089/user/login.do', 'post', {'usename': 'admin1', 'password': 'admin'}, '1'},
+#     {'http://localhost:8089/user/login.do', 'post', {'usename': 'admin', 'password': 'admin1'}, '1'}
+# ]
 
 
-run()
+
+def run(cases): # 传列表嵌套字典的测试用例
+
+    for item in cases:
+        print('开始执行:',item['desc'])
+        res =  HttpRequest().service(item['login_url'], item['method'], item['login_data'])
+        print('响应报文:', res.json())
+
+
+cases = [
+    {'login_url': 'http://localhost:8089/user/login.do', 'method': 'post', 'login_data': {"username": "admin", "password": "admin"}, 'desc': '正常登陆'},
+    {'login_url': 'http://localhost:8089/user/login.do', 'method': 'post', 'login_data': {"username": "admin1", "password": "admin"}, 'desc': '异常登陆'}
+]
+
+run(cases)
 
